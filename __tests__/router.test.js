a08bcccfe4c10037a6b8c8c6f67dd62fc7c72c4d
nwilliso <nwilliso@ucsd.edu>
2021-05-19 23:31:21 -0700

    Part 1 done

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,30 @@
  * @jest-environment jsdom
  */
 
+import { pushToHistory } from "../scripts/router";
+
+describe('pushToHistory test', () => {
+    test('no push', () => {
+        expect(history.state).toBe(null);
+        expect(history.length).toBe(1);
+    });
+    test('push entry', () => {
+        const x = Math.floor(Math.random * 10);
+        pushToHistory('entry', x);
+        expect(history.state.page).toBe('entry'+x);
+        expect(window.location.hash).toBe('#entry'+x)
+        expect(history.length).toBe(2);
+    });
+    test('push home', () => {
+        pushToHistory('',7);
+        expect(history.state.page).toBe(undefined);
+        expect(window.location.hash).toBe('')
+        expect(history.length).toBe(3);
+    });
+    test('push settings', () => {
+        pushToHistory('settings',0);
+        expect(history.state.page).toBe('settings');
+        expect(window.location.hash).toBe('#settings')
+        expect(history.length).toBe(4);
+    });
+});
